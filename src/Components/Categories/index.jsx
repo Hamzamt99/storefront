fc8c9a3fae977bacbb4e3484d1a4058c2245f9a4
Hamzamt99 <tamarihamza4@gmail.com>
2023-09-18 13:56:31 +0000

    done styling

--- src/Components/Categories/index.jsx
+++ src/Components/Categories/index.jsx
@@ -1,34 +1,51 @@
 import React from 'react'
 import { connect } from 'react-redux';
 import { dispatcher } from '../store/categories';
+import { deleteCart } from '../store/cart';
 import './style.scss'
+import { ChakraProvider } from '@chakra-ui/react';
+import { Grid, GridItem, Button, Stack } from '@chakra-ui/react'
 import { Link } from 'react-router-dom';
-// import { Box ,H} from '@chakra-ui/react';
 function Category(props) {
     const cartDetails = props.cart
     const categories = props.categories
     return (
         <>
-            <h1 >Browse Our Category</h1>
-            {/* <Box>TTTTTTTEst</Box> */}
-            <div className='category'>
+            <ChakraProvider>
+                <h1 >Browse Our Category</h1>
+                <div className='category'>
+                    {
+                        categories &&
+                        categories.map(category => {
+                            return (
+                                <Link key={category.name} onClick={() => props.dispatcher(category.name)}>
+                                    {category.name}
+                                </Link>
+                            )
+                        })
+                    }
+                </div>
                 {
-                    categories &&
-                    categories.map(category => {
-                        return (
-                            <Link key={category.name} onClick={() => props.dispatcher(category.name)}>
-                                {category.name}
-                            </Link>
-                        )
-                    })
+                    cartDetails.length &&
+                    < Grid
+                        h='200px'
+                        templateRows='repeat(2, 1fr)'
+                        templateColumns='repeat(5, 1fr)'
+                        gap={4}
+                    >
+                        <GridItem rowSpan={2} colSpan={1} bg='rgb(221, 221, 221)' > {
+                            cartDetails &&
+                            cartDetails.map((item, index) => {
+                                return [<h4>{item.name}</h4>,
+                                <Button colorScheme='red' size='xs' onClick={() => props.deleteCart(item)}>
+                                    Delete
+                                </Button>
+                                ]
+                            })
+                        }</GridItem>
+                    </Grid>
                 }
-            </div>
-
-            {cartDetails &&
-                cartDetails.map((item, index) => {
-                    return <p key={index}>{item.name}</p>
-                })}
-
+            </ChakraProvider >
         </>
     )
 }
@@ -38,6 +55,6 @@ const mapStateToProps = state => ({
     cart: state.cart.cart,
 })
 
-const mapDispatchToProps = { dispatcher }
+const mapDispatchToProps = { dispatcher, deleteCart }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Category)
